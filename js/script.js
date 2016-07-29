5efb7faebab23a26ed29ee69a02e1fcb4e7de49b
akim <kimandy703@gmail.com>
2016-07-29 12:00:02 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function()
+{
+
+
+  $('.breakfast').click(function(e){
+    $(this).toggleClass('fullscreen'); 
+  });
+
+  $('.lunch').click(function(e){
+    $(this).toggleClass('fullscreen'); 
+  });
+
+  $('.drinks').click(function(e){
+    $(this).toggleClass('fullscreen'); 
+  });
+
+  $('.salad').click(function(e){
+    $(this).toggleClass('fullscreen'); 
+  });
+
+console.log('working');
+});
